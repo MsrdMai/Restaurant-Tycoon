cc9b7f980505c221cf1f9cdfabe575b9957bf372
Fai Fai <42977918+SunisaChanbang@users.noreply.github.com>
2020-04-22 05:12:40 +0700

    Update script.js

--- homepage/script.js
+++ homepage/script.js
@@ -1,4 +1,5 @@
-
+$('#itemModal').modal('show');
+$('#cashModal').modal('show');
 document.addEventListener('DOMContentLoaded', function() {
     var initialLocaleCode = 'th';
     var calendarEl = document.getElementById('calendar');
@@ -13,6 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
       views: {
         listDay: { buttonText: 'list' }
       },
+      contentHeight: 550,
       locale: initialLocaleCode,
       nnavLinks: true, // can click day/week names to navigate views
       selectable: true,
@@ -20,25 +22,30 @@ document.addEventListener('DOMContentLoaded', function() {
       select: function(start, end) {
           // Display the modal.
           // You could fill in the start and end fields based on the parameters
-          $('.modal').modal('show');
+          $('#myModal').modal('show');
 
       },
       eventClick: function(event, element) {
                 // Display the modal and set the values to the event values.
-                $('.modal').modal('show');
-                $('.modal').find('#title').val(event.title);
-                $('.modal').find('#name').val(event.name);
-                $('.modal').find('#phone').val(event.phone);
-                $('.modal').find('#starts-at').val(event.start);
-                $('.modal').find('#amount').val(event.amount);
-                $('.modal').find('#table').val(event.table);
+                $('#myModal').modal('show');
+                $('#myModal').find('#title').val(event.title);
+                $('#myModal').find('#name').val(event.name);
+                $('#myModal').find('#phone').val(event.phone);
+                $('#myModal').find('#starts-at').val(event.start);
+                $('#myModal').find('#amount').val(event.amount);
+                $('#myModal').find('#table').val(event.table);
 
             },
             editable: true,
             eventLimit: true // allow "more" link when too many events
 
 });
-    $("#starts-at").datetimepicker({ footer: true, modal: true });
+
+    $("#starts-at").datetimepicker({ 
+        format: 'dd/mm/yyyy hh:mm tt',
+        footer: true,
+        modal: true 
+    });
     $('#save-event').on('click', function() {
             var title = $('#title').val();
             if (title) {
@@ -55,12 +62,48 @@ document.addEventListener('DOMContentLoaded', function() {
             $('#calendar').fullCalendar('unselect');
 
             // Clear modal inputs
-            $('.modal').find('input').val('');
+            $('#myModal').find('input').val('');
 
             // hide modal
-            $('.modal').modal('hide');
+            $('#myModal').modal('hide');
         });
 
     calendar.render();
 
   });
+// input number
+  $(document).ready(function() {
+    $('.count2').prop('disabled', true);
+    $('.minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 0 ? 0 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+        
+    });
+    $('.plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+});
+
+$(function () {
+
+  // INITIALIZE DATEPICKER PLUGIN
+  $('.datepicker').datepicker({
+      clearBtn: true,
+      language:'th-th',
+      format: " dd MM yyyy"
+  });
+
+
+  // FOR DEMO PURPOSE
+  $('#reservationDate').on('change', function () {
+      var pickedDate = $('input').val();
+      $('#pickedDate').html(pickedDate);
+  });
+});
